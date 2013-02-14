8f99dcee0102a4f8088e4ed62881f66a2d07a379
tom sela <tsela85@gmail.com>
2013-02-14 21:33:11 +0200

    fixed merge

--- codegen.scm
+++ codegen.scm
@@ -21,32 +21,33 @@
 
 (define code-gen
   (lambda (pe)
-    (display "codegen")
     (cond
           ((tag? 'const pe) (code-gen-const pe))
           ((tag? 'if-3 pe) (code-gen-if3 pe))
           ((tag? 'seq pe) (code-gen-seq pe))
           ((tag? 'or pe) (code-gen-or pe))
-		  ((tag? 'fvar pe) (code-gen-fvar pe))
- 		  ((tag? 'define pe) (code-gen-define pe))
+          ((tag? 'fvar pe) (code-gen-fvar pe))
+          ((tag? 'define pe) (code-gen-define pe))
           ((tag? 'pvar pe) (code-gen-pvar pe))
           ((tag? 'bvar pe) (code-gen-bvar pe))
           ((or (tag? 'tc-applic pe) (tag? 'applic pe)) (code-gen-applic pe))
           ((tag? 'lambda-simple pe) (code-gen-lambda-s pe))
+          ((tag? 'lambda-variadic pe) (code-gen-lambda-var pe))
+          ((tag? 'lambda-opt pe) (code-gen-lambda-opt pe))
           (else "not implemented")
           )))
 
 (define create-code
   (lambda (pe)
     (find-consts pe)
-	(create-buckets symbols)
-	(if (file-exists? "out.c")
-	    (delete-file "out.c"))
+        (create-buckets symbols)
+        (if (file-exists? "out.c")
+            (delete-file "out.c"))
     (let* ((out (open-output-file "out.c"))
-	       (mem-array (list->c-array (append const-list buckets)))
-	       (body (code-gen pe))
-		   
-	       (code (string-append
+               (mem-array (list->c-array (append const-list buckets)))
+               (body (code-gen pe))
+
+               (code (string-append
 "#include <stdio.h>
 #include <stdlib.h>
 #include \"cisc.h\"
@@ -60,26 +61,26 @@ int main()
 "  memcpy(&machine->mem[10],consts,sizeof(consts));
    MOV(ADDR(0), IMM("(number->string next-mem)"));
   void print_stack(char* comment){
-	int i;
-	printf(\"printing stack, FP: %d SP: %d %\\n\", (int)(FP), (int)(SP), comment);
-	for(i=SP+5; i>=0; --i){
-	if(SP == i){
-		printf(\"SP\");
-	}
-	if(FP == i){
-		printf(\"FP\");
-	}
-	printf(\"\\t element %d: \", i);
-	SHOW(\" \", STACK(i));
-	}
+        int i;
+        printf(\"printing stack, FP: %d SP: %d %\\n\", (int)(FP), (int)(SP), comment);
+        for(i=SP+5; i>=0; --i){
+        if(SP == i){
+                printf(\"SP\");
+        }
+        if(FP == i){
+                printf(\"FP\");
+        }
+        printf(\"\\t element %d: \", i);
+        SHOW(\" \", STACK(i));
+        }
 }
 void print_heap(){
-	int i;
-	printf(\"printing heap\\n\");
-	for (i=ADDR(0); i>=0; i--){
-		printf(\"\\t element %d: \", i);
-	SHOW(\" \",ADDR(i));
-	}
+        int i;
+        printf(\"printing heap\\n\");
+        for (i=ADDR(0); i>=0; i--){
+                printf(\"\\t element %d: \", i);
+        SHOW(\" \",ADDR(i));
+        }
 }
   #define SOB_VOID 10
   #define SOB_NIL 11
@@ -113,10 +114,10 @@ void print_heap(){
   ;"MOV(FP,SP);" nl
   "int i,j;" nl
   body nl
-  
-"  POP(FP); 
-  print_stack(\"dd\");
-  print_heap(\"dd\");
+
+"  POP(FP);
+//  print_stack(\"dd\");
+//  print_heap(\"dd\");
   PUSH(R0);
   CALL(WRITE_SOB);
   DROP(IMM(1));
@@ -128,136 +129,136 @@ error:
   printf(\"ERROR\\n\");
   STOP_MACHINE;
   return 1;}"))
-		   )
-		   (display code out)
-		   (close-output-port out))))
+                   )
+                   (display code out)
+                   (close-output-port out))))
 
 (define compile
   (lambda (e)
     (create-code (test e))))
-	
+
 (define code-gen-if3
   (lambda (e)
     (with e
-	  (lambda (if3 test do-if-true do-if-false)
-	    (let ((code-test (code-gen test))
-		      (code-dit (code-gen do-if-true))
-			  (code-dif (code-gen do-if-false))
-			  (label-else (^label-if3else))
-			  (label-exit (^label-if3exit)))
-			(string-append
-			code-test nl ; when run, the result of the test will be in R0
-			"CMP(R0, SOB_BOOLEAN_FALSE);" nl
-			"JUMP_EQ(" label-else ");" nl
-			code-dit nl
-			"JUMP(" label-exit ");" nl
-			label-else ":" nl
-			code-dif nl
-			label-exit ":" nl))))))
-			
+          (lambda (if3 test do-if-true do-if-false)
+            (let ((code-test (code-gen test))
+                      (code-dit (code-gen do-if-true))
+                          (code-dif (code-gen do-if-false))
+                          (label-else (^label-if3else))
+                          (label-exit (^label-if3exit)))
+                        (string-append
+                        code-test nl ; when run, the result of the test will be in R0
+                        "CMP(R0, SOB_BOOLEAN_FALSE);" nl
+                        "JUMP_EQ(" label-else ");" nl
+                        code-dit nl
+                        "JUMP(" label-exit ");" nl
+                        label-else ":" nl
+                        code-dif nl
+                        label-exit ":" nl))))))
+
 (define code-gen-const
   (lambda (e)
     (cond
-	 ((boolean? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
-	  ((number? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
-	  ((symbol? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
-	  (else 'error))))
-	  
+         ((boolean? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
+          ((number? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
+          ((symbol? (cadr e)) (string-append "MOV(R0," (number->string (lookup (cadr e) const-list)) ");"))
+          (else 'error))))
+
 (define code-gen-boolean
   (lambda (e)
-    (if e 
+    (if e
         "MOV(R0,SOB_BOOLEAN_TRUE);"
-		"MOV(R0,SOB_BOOLEAN_FALSE);")))
-		
+                "MOV(R0,SOB_BOOLEAN_FALSE);")))
+
 (define code-gen-int
   (lambda (e)
     (string-append
-	  "PUSH("(number->string e)");" nl
-	  "CALL(MAKE_SOB_INTEGER);" nl
-	  "DROP(IMM(1));")))
+          "PUSH("(number->string e)");" nl
+          "CALL(MAKE_SOB_INTEGER);" nl
+          "DROP(IMM(1));")))
 
 (define code-gen-seq
   (lambda (e)
     (with e
-	  (lambda (_ ex-list)
-	    (code-gen-seq-helper ex-list)))))
-		
+          (lambda (_ ex-list)
+            (code-gen-seq-helper ex-list)))))
+
 (define code-gen-seq-helper
   (lambda (e)
     (if (null? e)
-	    ""
-		(string-append (code-gen (car e)) nl (code-gen-seq-helper (cdr e))))))
-		
+            ""
+                (string-append (code-gen (car e)) nl (code-gen-seq-helper (cdr e))))))
+
 (define code-gen-or
   (lambda (e)
     (with e
-	  (lambda (_ ex-list)
-	    (let ((label-exit (^label-or-exit)))
-		  (string-append (code-gen-or-helper ex-list label-exit)
-		                 label-exit ":" nl))))))
-		  
+          (lambda (_ ex-list)
+            (let ((label-exit (^label-or-exit)))
+                  (string-append (code-gen-or-helper ex-list label-exit)
+                                 label-exit ":" nl))))))
+
 (define code-gen-or-helper
   (lambda (e label-exit)
     (if (null? e)
-	    "";(string-append label-exit ":")
-		(string-append (code-gen (car e)) nl 
-		"CMP(R0, SOB_BOOLEAN_FALSE);" nl
-		"JUMP_NE(" label-exit ");" nl
-		(code-gen-or-helper (cdr e) label-exit)))))
-		
+            "";(string-append label-exit ":")
+                (string-append (code-gen (car e)) nl
+                "CMP(R0, SOB_BOOLEAN_FALSE);" nl
+                "JUMP_NE(" label-exit ");" nl
+                (code-gen-or-helper (cdr e) label-exit)))))
+
 (define code-gen-pvar
   (lambda (e)
     (with e
-	  (lambda (_ v mi)
-	    (string-append 
-		"MOV(R0,FPARG(IMM(2+"(number->string mi)")));" nl)
-		))))
-		
+          (lambda (_ v mi)
+            (string-append
+                "MOV(R0,FPARG(IMM(2+"(number->string mi)")));" nl)
+                ))))
+
 (define code-gen-bvar
   (lambda (e)
     (with e
-	  (lambda (_ v maj mi)
-	    (string-append 
-		"//bvar: maj: "(number->string maj)", min: "(number->string mi) nl
-		"MOV(R0,FPARG(IMM(0)));" nl
-		"MOV(R0,INDD(R0,IMM("(number->string maj)")));" nl
-		"MOV(R0,INDD(R0,IMM("(number->string mi)")));" nl)
-		))))
-		
+          (lambda (_ v maj mi)
+            (string-append
+                "//bvar: maj: "(number->string maj)", min: "(number->string mi) nl
+                "MOV(R0,FPARG(IMM(0)));" nl
+                "MOV(R0,INDD(R0,IMM("(number->string maj)")));" nl
+                "MOV(R0,INDD(R0,IMM("(number->string mi)")));" nl)
+                ))))
+
 (define code-gen-fvar
   (lambda (e)
     (with e
-	  (lambda (_ sym)
-	     (let ((buck-addr (lookup sym buckets)))
-	    (string-append 
-		"//fvar: "(symbol->string sym) nl
-		"MOV(R0," (number->string buck-addr) ");" nl
-		"MOV(R0,INDD(R0,IMM(1)));" nl
-		"CMP(R0,0);" nl
-		"JUMP_EQ(error);" nl
-		
-		))))))
-		
+          (lambda (_ sym)
+             (let ((buck-addr (lookup sym buckets)))
+            (string-append
+                "//fvar: "(symbol->string sym) nl
+                "MOV(R0," (number->string buck-addr) ");" nl
+                "MOV(R0,INDD(R0,IMM(1)));" nl
+                "CMP(R0,0);" nl
+                "JUMP_EQ(error);" nl
+
+                ))))))
+
 (define code-gen-define
   (lambda (e)
     (with e
-	  (lambda (_ var val)
-	     (let* ((sym (cadr var))
-		        (buck-addr (lookup sym buckets)))
-	    (string-append 
-		"//define: "(symbol->string sym) nl
-		(code-gen val) nl
-		"MOV(R1," (number->string buck-addr) ");" nl
-		"MOV(INDD(R1,IMM(1)),R0);" nl
-		"MOV(R0,SOB_VOID);" nl
-			
-		))))))
-	    
-		
+          (lambda (_ var val)
+             (let* ((sym (cadr var))
+                        (buck-addr (lookup sym buckets)))
+            (string-append
+                "//define: "(symbol->string sym) nl
+                (code-gen val) nl
+                "MOV(R1," (number->string buck-addr) ");" nl
+                "MOV(INDD(R1,IMM(1)),R0);" nl
+                "MOV(R0,SOB_VOID);" nl
+
+                ))))))
+
+
 (define env-size 0)
-		
+
 (define code-gen-lambda-s
-  
+
   (lambda (e)
     (with e
           (lambda (_ args body)
@@ -510,7 +511,7 @@ error:
                 "//applic drop number of args" nl
 ;                "DROP(IMM("m"+2));" nl ; //TODO: m+2
                 "MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp " nl
-                "printf(\"drop: %d fp: %d\\n\",(int)(SP-IMM(STARG(IMM(0))+IMM(2))),(int)FP );" nl
+;                "printf(\"drop: %d fp: %d\\n\",(int)(SP-IMM(STARG(IMM(0))+IMM(2))),(int)FP );" nl
                  "DROP(IMM(STARG(IMM(0))+IMM(2)));" nl
 ;                "MOV(SP,FP); //TODO: remove" nl
 
@@ -552,7 +553,7 @@ error:
                "MOV(SP,R15 + 3 + IMM(STARG(IMM(0))));" nl
                "//tc-applic  get old fp to run over the old stack" nl
                "MOV(FP,R15)" nl
-  "printf(\"NEW FP: %d\\n \",FP); printf(\"NEW SP: %d\\n\",SP);" nl
+;  "printf(\"NEW FP: %d\\n \",FP); printf(\"NEW SP: %d\\n\",SP);" nl
                "JUMPA(INDD(R0,2));"nl
                ))))))
 
--- out.c
+++ out.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cisc.h"
-#define SHOW(msg, x) { printf("%s %s = %ld\n", (msg), (#x), (x)); } 
+#define SHOW(msg, x) { printf("%s %s = %ld\n", (msg), (#x), (x)); }
 
 
 int main()
@@ -11,26 +11,26 @@ int main()
   memcpy(&machine->mem[10],consts,sizeof(consts));
    MOV(ADDR(0), IMM(16));
   void print_stack(char* comment){
-	int i;
-	printf("printing stack, FP: %d SP: %d %\n", (int)(FP), (int)(SP), comment);
-	for(i=SP+5; i>=0; --i){
-	if(SP == i){
-		printf("SP");
-	}
-	if(FP == i){
-		printf("FP");
-	}
-	printf("\t element %d: ", i);
-	SHOW(" ", STACK(i));
-	}
+        int i;
+        printf("printing stack, FP: %d SP: %d %\n", (int)(FP), (int)(SP), comment);
+        for(i=SP+5; i>=0; --i){
+        if(SP == i){
+                printf("SP");
+        }
+        if(FP == i){
+                printf("FP");
+        }
+        printf("\t element %d: ", i);
+        SHOW(" ", STACK(i));
+        }
 }
 void print_heap(){
-	int i;
-	printf("printing heap\n");
-	for (i=ADDR(0); i>=0; i--){
-		printf("\t element %d: ", i);
-	SHOW(" ",ADDR(i));
-	}
+        int i;
+        printf("printing heap\n");
+        for (i=ADDR(0); i>=0; i--){
+                printf("\t element %d: ", i);
+        SHOW(" ",ADDR(i));
+        }
 }
   #define SOB_VOID 10
   #define SOB_NIL 11
@@ -61,12 +61,569 @@ PUSH(0);
 PUSH(FP);
 MOV(FP,SP);
 int i,j;
-PUSH(1);
-CALL(MAKE_SOB_INTEGER);
+//applic pushing args to stack
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(1));
+CALL(MALLOC);
 DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<0;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_9));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_9);
+L_clos_code_9:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //extending the env by 1
+//allocating space for the new env
+PUSH(IMM(2));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<1;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_10));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_10);
+L_clos_code_10:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //bvar: maj: 0, min: 0
+MOV(R0,FPARG(IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+
   POP(FP);
-  print_stack("dd");
-  print_heap("dd");
+  RETURN;
+L_clos_exit_10:
+
+  POP(FP);
+  RETURN;
+L_clos_exit_9:
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//applic pushing args to stack
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(1));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<0;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_8));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_8);
+L_clos_code_8:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //applic pushing args to stack
+MOV(R0,12);
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//applic pushing args to stack
+MOV(R0,14);
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+MOV(R0,FPARG(IMM(2+0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+  RETURN;
+L_clos_exit_8:
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//applic pushing args to stack
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(1));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<0;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_4));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_4);
+L_clos_code_4:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //applic pushing args to stack
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(2));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<1;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_5));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_5);
+L_clos_code_5:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //extending the env by 1
+//allocating space for the new env
+PUSH(IMM(3));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<2;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_6));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_6);
+L_clos_code_6:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //extending the env by 1
+//allocating space for the new env
+PUSH(IMM(4));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<3;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_7));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_7);
+L_clos_code_7:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //applic pushing args to stack
+//bvar: maj: 1, min: 0
+MOV(R0,FPARG(IMM(0)));
+MOV(R0,INDD(R0,IMM(1)));
+MOV(R0,INDD(R0,IMM(0)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//applic pushing args to stack
+//bvar: maj: 0, min: 0
+MOV(R0,FPARG(IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+MOV(R0,FPARG(IMM(2+0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+  RETURN;
+L_clos_exit_7:
+
+  POP(FP);
+  RETURN;
+L_clos_exit_6:
+
+  POP(FP);
+  RETURN;
+L_clos_exit_5:
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+MOV(R0,FPARG(IMM(2+0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+  RETURN;
+L_clos_exit_4:
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//applic pushing args to stack
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(1));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<0;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_2));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_2);
+L_clos_code_2:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //extending the env by 1
+//allocating space for the new env
+PUSH(IMM(2));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<1;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_3));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_3);
+L_clos_code_3:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //applic pushing args to stack
+//applic pushing args to stack
+MOV(R0,FPARG(IMM(2+0)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//bvar: maj: 0, min: 0
+MOV(R0,FPARG(IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//bvar: maj: 0, min: 0
+MOV(R0,FPARG(IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+MOV(R0,INDD(R0,IMM(0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+  RETURN;
+L_clos_exit_3:
+
+  POP(FP);
+  RETURN;
+L_clos_exit_2:
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+//extending the env by 1
+//allocating space for the new env
+PUSH(IMM(1));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R2,R0); //R2 <- new env
+//shifting the old enviroment
+MOV(R1,FPARG(IMM(0))); //R1 <- env
+//R2[j] <- R1[i]
+for(i=0,j=1;i<0;++i,++j){
+  MOV(INDD(R2,IMM(j)),INDD(R1,IMM(i)));
+}
+//moving params from the stack to the first list in env
+//allocating space
+PUSH(FPARG(IMM(1)));
+CALL(MALLOC);
+DROP(IMM(1));
+MOV(R3,R0); // R3 <- new env[0]
+for (i=0;i<FPARG(IMM(1));++i) { 
+  MOV(INDD(R3,i),FPARG((IMM(2+i)))); //R3[i] <- param[i]
+}
+MOV(INDD(R2,0),R3); // new env[0] <- R3
+PUSH(LABEL(L_clos_code_1));
+PUSH(R2);
+CALL(MAKE_SOB_CLOSURE);
+DROP(IMM(2));
+JUMP(L_clos_exit_1);
+L_clos_code_1:
+  PUSH(FP);
+  MOV(FP,SP);
+  //lambda-body
+  //applic pushing args to stack
+//applic pushing args to stack
+MOV(R0,FPARG(IMM(2+0)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+MOV(R0,FPARG(IMM(2+0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+PUSH(R0);
+
+//applic pushing number of args
+PUSH(IMM(1));
+MOV(R0,FPARG(IMM(2+0)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+  RETURN;
+L_clos_exit_1:
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+CMP(INDD(R0,0),T_CLOSURE);
+JUMP_NE(error);
+PUSH(INDD(R0,IMM(1)));//push clousre env
+CALLA(INDD(R0,IMM(2)));
+//applic drop number of args
+MOV(R10,IMM(STARG(IMM(0)))) //TODO: TEMP move sp 
+DROP(IMM(STARG(IMM(0))+IMM(2)));
+
+  POP(FP);
+//  print_stack("dd");
+//  print_heap("dd");
   PUSH(R0);
   CALL(WRITE_SOB);
   DROP(IMM(1));
