00dcc1ad8d250e52d5b461e8bd45b110fe70a915
tom sela <tsela85@gmail.com>
2013-03-01 22:26:40 +0200

    fixed makefile

--- lib/system/malloc.asm
+++ lib/system/malloc.asm
@@ -9,8 +9,6 @@
         PUSH(FP);
         MOV(FP, SP);
         CMP(ADDR(0),RAM_SIZE)   ;
-//        if(ADDR(0) == 63052)
-//                printf("malloc is: %d\n",ADDR(0)) ;
         JUMP_GE(MALLOC_HEAP_TOO_SMALL)               ;
         PUSH(R1);
         MOV(R1, FPARG(0));
--- makefile
+++ makefile
@@ -1,12 +1,6 @@
 all: out
 
-out: out.o
-	gcc -g -m32 -Wall -o out out.o
-
-out.o: out.c
-	gcc -m32 -g -Wall -ansi -c -o out.o out.c 
-
-.PHONY: clean
-
+out: out.c
+	gcc -g -m32 -o out out.c 
 clean: 
-	rm -f *.o out
+	rm -f out
